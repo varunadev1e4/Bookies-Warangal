61185d2f7f009648285dffbe7ab004d85df2407e
dumbdevelopergroot <varunaaruru13@gmail.com>
2026-03-11 13:24:13 +0530

    Fix session persisting in incognito windows

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -11,5 +11,6 @@ export const supabase = createClient(url, key, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
+    storage: window.sessionStorage,  // session dies when tab/window is closed
   }
 })
